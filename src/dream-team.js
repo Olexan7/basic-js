8592e2a0de921f524abb53d284227f7690b29090
olexan7 <ghoostt61@gmail.com>
2020-02-24 00:29:50 +0400

    feat: solve subtask Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  members = members.filter(n => {
+    return typeof n == "string";
+  });
+
+  members = members.map(item =>
+    item
+      .replace(/ /g, "")
+      .toUpperCase()
+      .charAt(0)
+  );
+
+  members = members.sort().join("");
+  return members;
 };
