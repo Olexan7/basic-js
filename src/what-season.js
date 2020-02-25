942525ce0f8664754a8398343ab01ff57bd80e3b
olexan7 <ghoostt61@gmail.com>
2020-02-25 22:22:33 +0400

    feat: add solve Tower of Hanoi

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,9 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let month = date.getMonth();
+  console.log(month);
+  if (month == 0 || month == 1 || month == 11) return "winter";
+  else if (month == 2 || month == 3 || month == 4) return "spring";
+  else if (moth == 5 || month == 6 || month == 7) return "summer";
+  else if (month == 8 || month == 9 || month == 10) return "autumn";
+  else throw new Error("Error");
 };
