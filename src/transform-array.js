c8613188c409256d39e66cdac01f1d25d49641d2
olexan7 <ghoostt61@gmail.com>
2020-03-04 00:07:06 +0400

    feat: add solve Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("Error");
+  if (arr.length == 0) return [];
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "--discard-next") {
+      i++;
+    } else if (arr[i] == "--discard-prev") {
+      newArr.pop();
+    } else if (arr[i] == "--double-next") {
+      if (i + 1 < arr.length) {
+        newArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] == "--double-prev") {
+      if (i != 0) {
+        newArr.push(arr[i - 1]);
+      }
+    } else if (
+      arr[i] != "--discard-prev" &&
+      arr[i] != "--double-prev" &&
+      arr[i] != "--double-next" &&
+      arr[i] != "--discard-next"
+    ) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
